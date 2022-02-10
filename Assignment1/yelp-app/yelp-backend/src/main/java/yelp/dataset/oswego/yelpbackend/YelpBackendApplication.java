8856c4f0f1d3519a424d17a6652146fd68d4b090
lgad31vn <lgad31vn@gmail.com>
2022-02-10 16:40:22 -0500

    remove vanila mvn project, added springboot

---
+++ Assignment1/yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/YelpBackendApplication.java
@@ -0,0 +1,13 @@
+package yelp.dataset.oswego.yelpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpBackendApplication.class, args);
+	}
+
+}
