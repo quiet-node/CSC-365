694b4ac3f9e377c19436cb001472a210e337cdb9
lgad31vn <lgad31vn@gmail.com>
2022-02-16 18:10:52 -0500

    init tailwind

--- Assignment1/yelp-app/yelp-frontend/src/App.tsx
+++ Assignment1/yelp-app/yelp-frontend/src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css';
-
-const App = () => {
-  return <div> Yelp App </div>;
-};
+function App() {
+  return (
+    <div className='text-2xl bg-violet-400 p-10 flex items-center justify-center'>
+      Yelp-App
+    </div>
+  );
+}
 
 export default App;
