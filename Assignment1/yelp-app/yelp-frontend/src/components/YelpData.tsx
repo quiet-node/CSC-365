48a6fe77687ef8463ce9ddf7b19032e52e448e66
lgad31vn <lgad31vn@gmail.com>
2022-02-16 19:19:11 -0500

    added custome JPA query, added one more endpoints to retrieve businessByName

--- Assignment1/yelp-app/yelp-frontend/src/components/YelpData.tsx
+++ Assignment1/yelp-app/yelp-frontend/src/components/YelpData.tsx
@@ -1,20 +1,36 @@
+import { useState } from 'react';
 import axios from 'axios';
 
-const getData = async (): Promise<void> => {
-  const res = await axios.get('http://localhost:8080/yelpdata');
-  console.log(res);
-};
-
 const YelpData = () => {
+  const [businessName, setBusinessName] = useState('');
+
+  const getData = async (): Promise<void> => {
+    const res = await axios.get(
+      `http://localhost:8080/yelpdata/${businessName}`
+    );
+
+    console.log(res.data);
+    console.log(businessName);
+  };
+
   return (
     <div className='bg-violet-200 min-h-screen w-full flex justify-center'>
       <div className='flex justify-center items-center'>
+        <input
+          type='text'
+          className='mr-5 border-[1px] border-green-400'
+          placeholder='Business Name...'
+          name='businessName'
+          value={businessName}
+          onChange={(e) => setBusinessName(e.target.value)}
+        />
+
         <button
           className='bg-blue-500 py-2 px-5 border-2 rounded-lg  '
           onClick={getData}
         >
           {' '}
-          click{' '}
+          Get Data{' '}
         </button>
       </div>
     </div>
