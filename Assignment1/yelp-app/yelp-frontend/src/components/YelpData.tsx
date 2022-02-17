60e1102d75fe9371160e7dde4a192daffb362031
lgad31vn <lgad31vn@gmail.com>
2022-02-16 20:28:37 -0500

    added getAllBusinesses

--- Assignment1/yelp-app/yelp-frontend/src/components/YelpData.tsx
+++ Assignment1/yelp-app/yelp-frontend/src/components/YelpData.tsx
@@ -4,33 +4,47 @@ import axios from 'axios';
 const YelpData = () => {
   const [businessName, setBusinessName] = useState('');
 
-  const getData = async (): Promise<void> => {
+  const getBusiness = async (): Promise<void> => {
     const res = await axios.get(
       `http://localhost:8080/yelpdata/${businessName}`
     );
 
+    res.data.map((business: any) => {
+      const { categories } = business;
+      console.log(categories);
+    });
+  };
+
+  const getAllBusinesses = async (): Promise<void> => {
+    const res = await axios.get('http://localhost:8080/yelpdata/allBusinesses');
+
     console.log(res.data);
-    console.log(businessName);
   };
 
   return (
     <div className='bg-violet-200 min-h-screen w-full flex justify-center'>
-      <div className='flex justify-center items-center'>
+      <div className='flex justify-center flex-col items-center'>
         <input
           type='text'
-          className='mr-5 border-[1px] border-green-400'
+          className='my-2 border-[1px] border-green-400'
           placeholder='Business Name...'
           name='businessName'
           value={businessName}
           onChange={(e) => setBusinessName(e.target.value)}
         />
-
         <button
           className='bg-blue-500 py-2 px-5 border-2 rounded-lg  '
-          onClick={getData}
+          onClick={getBusiness}
         >
           {' '}
-          Get Data{' '}
+          Get Business{' '}
+        </button>
+        <button
+          className='bg-blue-500 py-2 px-5 border-2 rounded-lg  '
+          onClick={getAllBusinesses}
+        >
+          {' '}
+          Get All Businesss{' '}
         </button>
       </div>
     </div>
