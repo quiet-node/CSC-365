7d02561a861a2ee7316b891aaa2c025b83c2fc37
lgad31vn <lgad31vn@gmail.com>
2022-02-21 23:08:21 -0500

    layout UI

--- Assignment1/yelp-app/yelp-frontend/src/components/YelpData.tsx
+++ Assignment1/yelp-app/yelp-frontend/src/components/YelpData.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import axios from 'axios';
+import SearchIcon from '@material-ui/icons/Search';
 
 const YelpData = () => {
   const [businessName, setBusinessName] = useState('');
 
-  const getSimilarBusinesses = async (): Promise<void> => {
+  const getSimilarBusinesses = async (e: any): Promise<void> => {
+    e.preventDefault();
     const res = await axios.get(
       `http://localhost:8080/yelpdata/similar/${businessName}`
     );
-    console.log(res.data);
+    setBusinessName('');
+    console.log('res.data');
   };
 
   const getAllBusinesses = async (): Promise<void> => {
@@ -18,30 +21,30 @@ const YelpData = () => {
   };
 
   return (
-    <div className='bg-violet-200 min-h-screen w-full flex justify-center'>
-      <div className='flex justify-center flex-col items-center'>
-        <input
-          type='text'
-          className='my-2 border-[1px] border-green-400'
-          placeholder='Business Name...'
-          name='businessName'
-          value={businessName}
-          onChange={(e) => setBusinessName(e.target.value)}
-        />
-        <button
-          className='bg-blue-500 py-2 px-5 border-2 rounded-lg  '
-          onClick={getSimilarBusinesses}
-        >
-          {' '}
-          Get Similar Businessses{' '}
-        </button>
-        <button
-          className='bg-blue-500 py-2 px-5 border-2 rounded-lg  '
-          onClick={getAllBusinesses}
-        >
-          {' '}
-          Get All Businessses{' '}
-        </button>
+    <div className=' bg-slate-200 min-h-screen w-full flex justify-center'>
+      <div className='flex w-full justify-center flex-col items-center'>
+        <div>Header</div>
+        <div className='flex h-4/6 w-full'>
+          <form
+            onSubmit={getSimilarBusinesses}
+            className=' flex ml-20 items-center'
+          >
+            <SearchIcon className='absolute mx-2 text-slate-400' />
+            <input
+              type='text'
+              className='py-[7px] pl-10 border-[1px] shadow-lg rounded-md outline-0 text-gray-400'
+              placeholder='Business Name...'
+              name='businessName'
+              value={businessName}
+              onChange={(e) => setBusinessName(e.target.value)}
+            />
+            <button type='submit'></button>
+          </form>
+
+          <div className='flex justify-center items-center w-3/5'>
+            Business List{' '}
+          </div>
+        </div>
       </div>
     </div>
   );
